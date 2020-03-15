f8c9a00aaad029cea070682244c562adb81a529e
Yves Bertot <yves.bertot@inria.fr>
2020-03-15 08:45:07 +0100

    finished the termination proof, added an explanation of the main proof.

--- sqrt/model_sqrt1.v
+++ sqrt/model_sqrt1.v
@@ -1,14 +1,21 @@
 From compcert Require Import Core Floats Defs Bits Binary.
 Require Import Reals Psatz.
+Require Import FunInd Recdef.
 
 Open Scope Z_scope.
 
 Definition float_to_nat (z: float32) : nat :=
   match z with
-   | B754_zero _ => O
-   | B754_infinity _ => O
+   | B754_zero _ => 2 ^ Z.to_nat 280
+   | B754_infinity sign => if sign then 0 else 2 ^ Z.to_nat 600
    | B754_nan _ _ _ => O
-   | B754_finite _ m e _ => Pos.to_nat m * 2 ^ Z.to_nat(e + 149)
+   | B754_finite sign m e _ =>
+     if sign then
+        2 ^ Z.to_nat 280
+        - Pos.to_nat m * 2 ^ Z.to_nat(e + 149)
+     else
+        2 ^ Z.to_nat 280
+        + Pos.to_nat m * 2 ^ Z.to_nat(e + 149)
    end.
 
 Definition float_to_exp (z : float32) : Z :=
@@ -22,7 +29,7 @@ Proof.
 apply Rabs_pos_eq, pos_INR.
 Qed.
 
-Lemma Zpow_natpow (x y : nat) : Z.of_nat (x ^ y) =
+Lemma natpow_Zpow (x y : nat) : Z.of_nat (x ^ y) =
   Z.of_nat x ^ Z.of_nat y.
 Proof.
 induction y as [ | y IH].
@@ -39,34 +46,262 @@ destruct n as [ | p | p]; auto; try lia.
 now unfold Z.to_nat, IZR; rewrite INR_IPR.
 Qed.
 
-Lemma float_to_nat_prop (z : float32) :
-  is_finite 24 128 z = true -> (-149 <= float_to_exp z) ->
-  INR (float_to_nat z) = Rabs (B2R 24 128 z  * bpow radix2 149).
+Lemma bounded_bound_exp m e : 
+  Binary.bounded 24 128 m e = true -> -149 <= e <= 104.
 Proof.
-intros fin1 fin2.
-destruct z as [dummy | dummy | dum1 dum2 | sign m e validity];
-  try discriminate fin1.
-- now rewrite Rmult_0_l, Rabs_R0.
-- unfold float_to_nat, B2R.
-  rewrite F2R_cond_Zopp; unfold F2R; simpl IZR; simpl Fexp.
-  rewrite !Rabs_mult, abs_cond_Ropp, Rabs_mult, mult_INR.
-  replace (Rabs (IZR (Z.pos m))) with (INR (Pos.to_nat m)); cycle 1.
-    rewrite INR_IPR.
-    rewrite Rabs_pos_eq;[reflexivity | apply IZR_le; lia].
-  rewrite !Rabs_pos_eq; try apply bpow_ge_0.
-  rewrite Rmult_assoc, <- bpow_plus.
-  rewrite (INR_IZR_INZ (2 ^ _)).
-  rewrite Zpow_natpow, <- pow_IZR.
-  replace (IZR (Z.of_nat 2)) with 2%R by reflexivity.
-  rewrite <- Rpower_pow;[ | lra].
-  rewrite bpow_powerRZ, powerRZ_Rpower; simpl;[ | lra].
-  rewrite !IZR_INR_Z2N; [easy | | lia].
-  simpl in fin2; lia.
+intros vc; unfold Binary.bounded in vc.
+destruct (andb_prop _ _ vc) as [vc1 vc2].
+apply (canonical_canonical_mantissa _ _ false) in vc1.
+apply Zle_bool_imp_le in vc2.
+split;[simpl in vc1 |- *; clear vc vc2 | exact vc2].
+revert vc1.
+unfold canonical, F2R, cexp; simpl; unfold FLT_exp.
+destruct (mag radix2 (IZR (Z.pos m) * bpow radix2 e) - 24);
+  intros v; rewrite v; apply Z.le_max_r.  
 Qed.
+
+Lemma bound_exp_float32 (z : float32) :
+  -149 <= float_to_exp z <= 104.
+Proof.
+destruct z as [dummy | dummy | dum1 dum2 dum3 | sign m e vc];
+  try (split; intros; discriminate).
+now simpl; apply (bounded_bound_exp m).
+Qed.
+
+Transparent Float32.cmp.
+
+Lemma pow2_pos x : (0 < 2 ^ x)%nat.
+Proof.  induction x as [ | x IH]; try (simpl; lia).  Qed.
+
+Lemma Zto_nat_pow x y : 0 <= x -> 0 <= y -> (Z.to_nat (x ^ y)) = 
+         (Z.to_nat x ^ Z.to_nat y)%nat.
+Proof.
+intros x0 y0.
+pattern y; apply Zlt_0_ind;[clear y y0| exact y0].
+intros y IH y0.
+apply Zle_lt_or_eq in y0; destruct y0 as [ygt0 | yeq0].
+  assert (0 <= (y - 1)) by lia.
+  assert (y1 : y = y - 1 + 1) by ring.
+  rewrite y1, Z.pow_add_r, Z.pow_1_r, Z2Nat.inj_mul; try lia.
+    rewrite Z2Nat.inj_add; try lia.  
+    rewrite Nat.pow_add_r.
+    rewrite IH; try lia.
+  replace (Z.to_nat 1) with 1%nat by reflexivity.
+  rewrite Nat.pow_1_r; reflexivity.
+now apply Z.pow_nonneg.
+rewrite <- yeq0; reflexivity.
+Qed.
+
+Lemma bound_mantissa_digits m k :
+  Z.pos (Digits.digits2_pos m) <= k ->
+  (Pos.to_nat m < 2 ^ Z.to_nat k)%nat.
+Proof.
+intros nd.
+assert (0 <= k).
+  apply Z.le_trans with (2 := nd); lia.
+rewrite Digits.Zpos_digits2_pos in nd.
+replace (Pos.to_nat m) with (Z.to_nat (Z.pos m)) by reflexivity; try lia.
+replace (2 ^ Z.to_nat k)%nat with (Z.to_nat (2 ^ k)) by ( apply Zto_nat_pow; lia).
+  rewrite <- Z2Nat.inj_lt; try lia.
+  apply (Digits.Zpower_gt_Zdigits radix2 k (Z.pos m)).
+  lia.
+now apply Z.pow_nonneg.
+Qed.
+
+Lemma lower_bound_mantissa_digits m :
+  (2 ^ (Pos.to_nat (Digits.digits2_pos m) - 1) <= Pos.to_nat m)%nat.
+Proof.
+replace (Pos.to_nat m) with (Z.to_nat (Z.pos m)) by reflexivity.
+replace (Pos.to_nat (Digits.digits2_pos m)) with
+  (Z.to_nat (Z.pos (Digits.digits2_pos m))) by reflexivity.
+replace 1%nat with (Z.to_nat 1) at 2 by reflexivity.
+rewrite <- Z2Nat.inj_sub;[ | lia].
+replace (2 ^ Z.to_nat (Z.pos (Digits.digits2_pos m) - 1))%nat with
+  (Z.to_nat (2 ^ (Z.pos (Digits.digits2_pos m) - 1)))
+   by ( apply Zto_nat_pow; lia).
+rewrite <- Z2Nat.inj_le;[ | apply Z.lt_le_incl, Z.pow_pos_nonneg; lia | lia].
+rewrite Digits.Zpos_digits2_pos.
+rewrite <- (Z.abs_eq (Z.pos m)) at 2;[ | lia].
+apply (Digits.Zpower_le_Zdigits radix2); lia.
+Qed.
+
+Lemma bound_mantissa_nat m e :
+  Binary.bounded 24 128 m e = true ->
+  (Pos.to_nat m < 2 ^ 24)%nat.
+Proof.
+intros v.
+apply (bound_mantissa_digits m 24); try lia.
+unfold Binary.bounded in v.
+unfold canonical_mantissa in v.
+apply andb_prop in v; destruct v as [v _].
+apply Zeq_bool_eq in v; unfold FLT_exp in v.
+destruct (Z_le_gt_dec (3 - 128 - 24) (Z.pos (Digits.digits2_pos m) + e - 24))
+      as [l | r].
+  rewrite Z.max_l in v; [lia | assumption].
+lia.
+Qed.
+
+Lemma lower_bound_mantissa_nat e m :
+  -149 < e ->
+  Binary.bounded 24 128 m e = true ->
+  (2 ^ 23 <= Pos.to_nat m)%nat.
+Proof.
+intros elb v.
+apply le_trans with (2 := lower_bound_mantissa_digits m).
+unfold Binary.bounded in v.
+unfold canonical_mantissa in v.
+apply andb_prop in v; destruct v as [v _].
+apply Zeq_bool_eq in v; unfold FLT_exp in v.
+destruct (Z_le_gt_dec (3 - 128 - 24) (Z.pos (Digits.digits2_pos m) + e - 24))
+      as [l | r].
+  rewrite Z.max_l in v; [ | assumption].
+  assert (vd : Z.pos (Digits.digits2_pos m) = 24) by lia.
+  injection vd; intros vd'; rewrite vd'.
+  now apply Nat.pow_le_mono_r.
+lia.
+Qed.
+
+Lemma bound_float : forall x e, Binary.bounded 24 128 x e = true ->
+              (Pos.to_nat x * 2 ^ Z.to_nat (e + 149) < 2 ^ Z.to_nat 280)%nat.
+Proof.
+intros x e v.
+apply lt_trans with (2 ^ 24 * 2 ^ Z.to_nat (e + 149))%nat.
+apply Nat.mul_lt_mono_pos_r.
+    now apply pow2_pos.
+  now apply (bound_mantissa_nat x e).
+rewrite <- Nat.pow_add_r.
+apply Nat.pow_lt_mono_r; try lia.
+assert (tmp := bounded_bound_exp _ _ v); lia.
+Qed.
+
+
+Lemma float_to_nat_Clt a b :
+  Float32.cmp Integers.Clt a b = true ->
+    (float_to_nat a < float_to_nat b)%nat.
+Proof.
+destruct a as [da | da | da1 da2 da3 | signa ma ea vca];
+  destruct b as [db | db | db1 db2 db3 | signb mb eb vcb]; try
+  discriminate.
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case db;[discriminate | intros _; apply Nat.pow_lt_mono_r; lia].
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case signb;[ discriminate | intros].
+  apply Nat.le_lt_trans with (2 ^ Z.to_nat 280 + 0)%nat.
+    now rewrite Nat.add_0_r; apply le_n.
+  apply Nat.add_lt_mono_l, Nat.mul_pos_pos.
+    now apply Pos2Nat.is_pos.  
+  now apply pow2_pos.
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case da; [now intros; apply pow2_pos | discriminate ].
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  now case da; case db; try discriminate; intros; apply pow2_pos.
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case da;[intros | discriminate].
+  case signb.
+    apply lt_minus_O_lt.
+    now apply bound_float.
+  apply Nat.add_pos_r.
+  apply Nat.mul_pos; split;[lia | apply pow2_pos].
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case signa;[ intros | discriminate].
+  assert (tech : forall a b, (0 < a -> 0 < b -> a - b < a)%nat).
+    intros a b a0 b0; destruct a as [ | a]; destruct b as [ | b]; lia.
+  apply tech;[| apply Nat.mul_pos; split;[lia |]]; apply pow2_pos.
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case db;[discriminate | intros _].
+  case signa.
+    apply lt_trans with (2 ^ Z.to_nat 280)%nat.
+  assert (tech : forall a b, (0 < a -> 0 < b -> a - b < a)%nat).
+    intros a b a0 b0; destruct a as [ | a]; destruct b as [ | b]; lia.
+  apply tech;[| apply Nat.mul_pos; split;[lia |]]; apply pow2_pos.
+  apply Nat.pow_lt_mono_r; lia.  
+  apply lt_trans with (2 ^ Z.to_nat 280 + 2 ^ Z.to_nat 280)%nat.
+    apply Nat.add_lt_mono_l.
+    now apply bound_float.
+  assert (tech : forall x, (x + x = 2 ^ 1 * x)%nat) by (intros; simpl; ring).
+  rewrite tech, <- Nat.pow_add_r; apply Nat.pow_lt_mono_r; lia.
+- unfold Float32.cmp, Float32.compare, Bcompare, cmp_of_comparison, float_to_nat.
+  case signa; case signb; try discriminate.
+  * assert (tech : (forall a b c, b <= a -> c <= a -> c < b -> a - b < a - c)%nat).
+      intros a b c ba ca cb; lia.
+    destruct (ea ?= eb) eqn:eaeb; try discriminate.
+     destruct (CompOpp (Pos.compare_cont Eq ma mb)) eqn:mamb; try discriminate.
+     destruct (Pos.compare_cont Eq ma mb) eqn: mamb'; try discriminate.
+     apply nat_of_P_gt_Gt_compare_morphism in mamb'.
+     intros _; apply tech.
+         now apply Nat.lt_le_incl, bound_float.
+       now apply Nat.lt_le_incl, bound_float.
+     rewrite (Z.compare_eq _ _ eaeb).
+     rewrite <- Nat.mul_lt_mono_pos_r;[lia | apply pow2_pos].
+    intros _; apply tech.
+        now apply Nat.lt_le_incl, bound_float.
+      now apply Nat.lt_le_incl, bound_float.
+    rewrite Z.compare_gt_iff in eaeb.
+    replace ea with (ea - eb + eb) by ring.
+    assert (ebb := bounded_bound_exp _ _ vcb).
+    rewrite <- Z.add_assoc, (Z2Nat.inj_add (ea - eb)), Nat.pow_add_r; try lia.
+    rewrite Nat.mul_assoc; apply Nat.mul_lt_mono_pos_r; try apply pow2_pos.
+    apply Nat.lt_le_trans with (1 := bound_mantissa_nat _ _ vcb).
+    apply Nat.le_trans with ((2 ^ 23) * 2 ^ Z.to_nat (ea - eb))%nat; cycle 1.
+      apply Nat.mul_le_mono_pos_r;[now apply pow2_pos | ].
+      apply (lower_bound_mantissa_nat ea);[lia | assumption].
+    replace (ea - eb) with (1 + (ea - eb -1)) by ring.
+    rewrite Nat.pow_succ_r', Nat.mul_comm.
+    apply Nat.mul_le_mono_l; rewrite Z2Nat.inj_add;[ | lia | lia].
+    rewrite Nat.pow_add_r.
+    replace 2%nat with (2 * 1)%nat at 1 by ring.
+    now apply Nat.mul_le_mono_l, pow2_pos.
+  * intros _.
+    apply bound_float in vca; apply bound_float in vcb.
+    enough (0 < Pos.to_nat mb * 2 ^ Z.to_nat (eb + 149) /\
+            0 < Pos.to_nat ma * 2 ^ Z.to_nat (ea + 149))%nat by lia.
+    split; apply Nat.mul_pos_pos; try apply pow2_pos; lia.
+  * destruct (ea ?= eb) eqn:eaeb; try discriminate.
+     destruct (Pos.compare_cont Eq ma mb) eqn:mamb; try discriminate.
+     apply nat_of_P_lt_Lt_compare_morphism in mamb.
+     intros _; apply Nat.add_lt_mono_l.
+     rewrite (Z.compare_eq _ _ eaeb).
+     rewrite <- Nat.mul_lt_mono_pos_r;[lia | apply pow2_pos].
+    intros _; apply Nat.add_lt_mono_l.
+    rewrite Z.compare_lt_iff in eaeb.
+    replace eb with (eb - ea + ea) by ring.
+    assert (eba := bounded_bound_exp _ _ vca).
+    rewrite <- Z.add_assoc, (Z2Nat.inj_add (eb - ea)), Nat.pow_add_r; try lia.
+    rewrite Nat.mul_assoc; apply Nat.mul_lt_mono_pos_r; try apply pow2_pos.
+    apply Nat.lt_le_trans with (1 := bound_mantissa_nat _ _ vca).
+    apply Nat.le_trans with ((2 ^ 23) * 2 ^ Z.to_nat (eb - ea))%nat; cycle 1.
+      apply Nat.mul_le_mono_pos_r;[now apply pow2_pos | ].
+      apply (lower_bound_mantissa_nat eb);[lia | assumption].
+    replace (eb - ea) with (1 + (eb - ea -1)) by ring.
+    rewrite Nat.pow_succ_r', Nat.mul_comm.
+    apply Nat.mul_le_mono_l; rewrite Z2Nat.inj_add;[ | lia | lia].
+    rewrite Nat.pow_add_r.
+    replace 2%nat with (2 * 1)%nat at 1 by ring.
+    now apply Nat.mul_le_mono_l, pow2_pos.
+Qed.
+
+Definition main_loop_measure (p : float32 * float32) : nat :=
+  float_to_nat (snd p).
 
 Definition body_exp x y :=
   Float32.div (Float32.add y (Float32.div x y))
      (Float32.of_int (Integers.Int.repr 2)).
+
+
+(* The beauty of it is that we don't need to know what computation is performed,
+  only the test matters. *)
+Function main_loop (p : float32 * float32) {measure main_loop_measure} :
+   float32 :=
+  match p with
+  | (x, y) =>
+    if Float32.cmp Integers.Clt (body_exp x y) y then
+       main_loop (x, body_exp x y)
+    else
+      body_exp x y
+  end.
+Proof.
+now intros p x y eqxy; apply float_to_nat_Clt.
+Qed.
 
 Transparent Float32.div.
 Transparent Float32.add.
@@ -763,7 +998,7 @@ Proof.
 intros x' y' inty' intx' stop.
 assert (st3 : (Rabs (body_exp_R x' y' - ((y' + (x' / y')) / 2)) <= 8 * ulp1)%R).
   admit.
-Qed.
+Admitted.
 
 Lemma target_above_s x y :
   let x' := B2R 24 128 x in
